6b36be5779536d3a64399ad41e037bc31d05acba
Sean <seanzzz@Seans-Mac-Studio.local>
2023-03-23 23:56:37 -0600

    Connect with db

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets 
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer 
 # Create your views here.
+
+class ProductViewSet(viewsets.ViewSet): 
+    def list(self, request): #/api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request): #/api/products
+        pass 
+
+    def retrieve(self, request, pk=None): #/api/products/<str:id>
+        pass 
+
+    def update(self, request, pk=None): #/api/products/<str:id>
+        pass 
+    
+    def destroy(self, request, pk=None): #/api/products/<str:id>
+        pass 
